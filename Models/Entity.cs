7f2efd427a1ec3436ebdaf18c9261783a188f7a7
huseynManaf <huseynim@code.edu.az>
2023-01-31 07:51:46 -0800

    init

---
+++ Models/Entity.cs
@@ -0,0 +1,7 @@
+﻿namespace P329ConsoleAppPractise.Models
+{
+    internal class Entity
+    {
+        internal int Id { get; set; }
+    }
+}
---
+++ Services/Contracts/ICrudService.cs
@@ -0,0 +1,13 @@
+﻿using P329ConsoleAppPractise.Models;
+
+namespace P329ConsoleAppPractise.Services.Contracts
+{
+    internal interface ICrudService
+    {
+        void Add(Entity entity);
+        void Update(int id, Entity entity);
+        void Delete(int id);
+        Entity Get(int id);
+        Entity[] GetAll();
+    }
+}
